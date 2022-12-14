09a8d9a1296319933489cf597cac1943577cfc12
Afaq Khan <101743351+afaq-khan2000@users.noreply.github.com>
2022-12-15 00:10:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ var tradeFinancesRouter = require('./routes/tradeFinances');
 var app = express();
 
 var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  "origin": 'http://localhost:3000',
+  "optionsSuccessStatus": 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.all(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.static('public'));
 
